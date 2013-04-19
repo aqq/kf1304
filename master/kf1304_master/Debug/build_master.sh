fbb1587792f12943eca9fc7b8312d65ba2147361
aqq <568264099@qq.com>
2013-04-19 09:25:22 +0800

    1.fix build_master.sh

--- master/kf1304_master/Debug/build_master.sh
+++ master/kf1304_master/Debug/build_master.sh
@@ -1,15 +1,7 @@
-echo g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Cpp2mysql.d" -MT"src/Cpp2mysql.d" -o "src/Cpp2mysql.o" "../src/Cpp2mysql.cpp"
+make clean
+make all
+cd ..
+echo "cd .."
+mv Debug/kf1304_master kf1304_master
+echo "mv Debug/kf1304_master kf1304_master"
 
-echo g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/GlobalHelper.d" -MT"src/GlobalHelper.d" -o "src/GlobalHelper.o" "../src/GlobalHelper.cpp"
-
-echo g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/kf1304_master.d" -MT"src/kf1304_master.d" -o "src/kf1304_master.o" "../src/kf1304_master.cpp"
-
-echo g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/master.d" -MT"src/master.d" -o "src/master.o" "../src/master.cpp"
-
-echo g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/slaver.d" -MT"src/slaver.d" -o "src/slaver.o" "../src/slaver.cpp"
-
-echo g++  -o "kf1304_master"  ./src/Cpp2mysql.o ./src/GlobalHelper.o ./src/kf1304_master.o ./src/master.o ./src/slaver.o   -lmysqlclient
- 
-echo cd ..
-echo cp Debug/kf1304_master kf1304_master
-echo "ok ./kf1304_master"
